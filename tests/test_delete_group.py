a381c469bc5f4d06e3e3d3378f5dcfffadd1feb5
elenay <magique79@gmail.com>
2015-03-14 23:07:55 +0200

    fixed name of function

--- tests/test_delete_group.py
+++ tests/test_delete_group.py
@@ -1,7 +1,7 @@
 __author__ = 'eya'
 
 
-def test_add_group(app):
+def test_delete_group(app):
     app.session.login(username="admin", password="secret")
     app.group.delete_first_group()
     app.session.logout()
