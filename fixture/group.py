39704fe4d75a5d3c51aa1109f3cc5d3d52ab082c
elenay <magique79@gmail.com>
2015-03-14 23:00:35 +0200

    Made fixture common for all tests

--- fixture/group.py
+++ fixture/group.py
@@ -27,3 +27,11 @@ class GroupHelper:
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         #submit group form
         wd.find_element_by_name("submit").click()
+
+    def delete_first_group(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        #select first group
+        wd.find_element_by_name("selected[]").click()
+        #submit deletion
+        wd.find_element_by_name("delete").click()
--- tests/test_add_contact.py
+++ tests/test_add_contact.py
@@ -1,14 +1,6 @@
 __author__ = 'eya'
 
-import pytest
 from model.contact import Contact
-from fixture.application import Application
-
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 def test_add_contact(app):
     app.open_home_page()
