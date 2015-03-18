b6aa5ea1c730df4f2f1e3668791e8febed9d464e
elenay <magique79@gmail.com>
2015-03-18 18:19:45 +0200

    Fixed unnecessary pages navigation

--- tests/test_delete_contact.py
+++ tests/test_delete_contact.py
@@ -4,6 +4,5 @@ from model.contact import Contact
 
 def test_delete_contact(app):
     if app.contact.count() == 0:
-        app.contact.open_add_contact_page()
         app.contact.create(Contact(firstname = "testone"))
     app.contact.delete_first_contact()
--- tests/test_modify_contact.py
+++ tests/test_modify_contact.py
@@ -4,6 +4,5 @@ from model.contact import Contact
 
 def test_modify_contact(app):
     if app.contact.count() == 0:
-        app.contact.open_add_contact_page()
         app.contact.create(Contact(firstname="testone"))
     app.contact.edit_first_contact(Contact(firstname="edited-one"))
