3b7023313000618f2f067b6dde0e18cf4edd30bd
elenay <magique79@gmail.com>
2015-03-27 17:56:48 +0200

    Developed groups list checks

--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -1,9 +1,21 @@
 __author__ = 'eya'
-
+from sys import maxsize
 from model.group import Group
 
 def test_add_group(app):
-    app.group.create(Group(name="finn", header="four", footer="tour"))
+    old_groups = app.group.get_group_list()
+    group = Group(name="finn", header="four", footer="tour")
+    app.group.create(group)
+    new_groups = app.group.get_group_list()
+    assert len(old_groups)+1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 def test_add_empty_group(app):
-    app.group.create(Group(name="", header="", footer=""))
+    old_groups = app.group.get_group_list()
+    group = Group(name="", header="", footer="")
+    app.group.create(group)
+    new_groups = app.group.get_group_list()
+    assert len(old_groups)+1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
