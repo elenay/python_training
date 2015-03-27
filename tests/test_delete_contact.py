a2bb8d0b63f616283405c84510c2c88c45764012
elenay <elena.yermakova@chloeandisabel.com>
2015-03-27 23:53:38 +0200

    Changed deletion of element from array to less brutal way

--- tests/test_delete_contact.py
+++ tests/test_delete_contact.py
@@ -11,5 +11,5 @@ def test_delete_contact(app):
     app.contact.delete_first_contact()
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts)-1 == len(new_contacts)
-    old_contacts[0:1] = []
+    del old_contacts[0]
     assert old_contacts == new_contacts
