cd528d10a35c3dfe1e7b1518fe50322c3d6ebfd9
elenay <magique79@gmail.com>
2015-04-09 00:05:09 +0300

    Added loading test data from json file to group test
    contact test to go

---
+++ data/groups.py
@@ -0,0 +1,9 @@
+__author__ = 'magic'
+from model.group import Group
+
+
+testdata = [
+    Group(name="Name1", header="header1", footer="footer1"),
+    Group(name="Name1", header="header1", footer="footer1")
+]
+
