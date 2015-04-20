8f188a5c35b669f76e7f097d27f7534fee721ced
elenay <magique79@gmail.com>
2015-04-20 14:10:19 +0300

    Added check db contacts and contacts from home page

--- tests/test_all_contact_fields.py
+++ tests/test_all_contact_fields.py
@@ -3,14 +3,20 @@ import re
 from model.contact import Contact
 
 
-def test_all_fields_on_home_page(app, db):
-    contacts_from_home_page = db.get_contact_list()
-    index = range(len(contacts_from_home_page))
+def test_all_fields_on_edit_page(app, db):
+    contacts_from_db = db.get_contact_list()
+    index = range(len(contacts_from_db))
     contacts_from_edit_page = []
     for i in index:
         contact_from_edit_page = app.contact.get_contact_info_from_edit_page(i)
         contacts_from_edit_page.append(contact_from_edit_page)
-    assert sorted(contacts_from_home_page, key=Contact.id_or_max) == sorted(contacts_from_edit_page, key=Contact.id_or_max)
+    assert sorted(contacts_from_db, key=Contact.id_or_max) == sorted(contacts_from_edit_page, key=Contact.id_or_max)
+
+
+def test_all_fields_on_home_page(app, db):
+    contacts_from_home_page = app.contact.get_contact_list()
+    db_contacts = db.get_contact_list()
+    assert sorted(contacts_from_home_page, key=Contact.id_or_max) == sorted(db_contacts, key=Contact.id_or_max)
 
 
 def clear(s):
