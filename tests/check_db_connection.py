0588a6d41e2c58f37eeee0bab90adb632e943d15
elenay <magique79@gmail.com>
2015-04-28 23:01:31 +0300

    WIP changed getting lists from db

--- tests/check_db_connection.py
+++ tests/check_db_connection.py
@@ -5,9 +5,22 @@ from model.group import Group
 db = OrmFixture(host="127.0.0.1", name="addressbook", user="root", password="")
 
 try:
-    l = db.get_contacts_not_in_group(Group(id="168"))
-    for item in l:
-        print(item)
-    print (len(l))
+    i = []
+    groups = db.get_group_list()
+    print(groups)
+    for g in groups:
+        # group = groups[g]
+        # group_id = group[:3]
+        group_id = Group.id
+        i.append(group_id)
+    print (i)
+    # for item in db.get_group_list:
+    #     i.append(db.get_group_list[0])
+    # l = []
+    # for ids in i:
+    #     l = db.get_contacts_in_any_group(Group(id=i))
+    # for item in l:
+    #     print(item)
+    # print (len(l))
 finally:
     pass #db.destroy()
